b3185d01891709abdff132b4bcda8df99dca42ea
Stéphane Dauguet <sdauguet@gmail.com>
2021-11-20 18:23:02 +0100

    add loader

---
+++ src/components/utils/Loading.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div
+      style={{ minHeight: "100vh" }}
+      className="d-flex flex-row justify-content-center align-items-center w-100"
+    >
+      <img
+        src="https://media2.giphy.com/media/l4FGv5Ci0WIp8kYhO/giphy.gif?cid=ecf05e477ro3p6fjztik5jpulr9bhzrxrdh818cqzmaye6ju&rid=giphy.gif&ct=g"
+        alt="Loading gif"
+      />
+    </div>
+  );
+};
+
+export default Loading;
